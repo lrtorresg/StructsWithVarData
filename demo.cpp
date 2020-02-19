bf0f25cd465bc4c7e6d80c0109546baff21b04ad
Luis Ricardo Torres Garcia <luis@linux-0qew.home>
2020-02-19 06:34:55 +0100

    adding some code to demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,4 +1,26 @@
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+
+
+struct Parameters
+{	
+	int32_t varDataSize;
+	int32_t* pVarData;
+};	
+
+
 int main()
 {
-  return 0;
+	Parameters paramsA;
+	paramsA.varDataSize = 10;
+	paramsA.pVarData = new int[10];
+	std::fill(paramsA.pVarData, paramsA.pVarData + 10, 1988);
+	
+	auto paramsB = paramsA;
+	assert(std::all_of(paramsB.pVarData, paramsB.pVarData + 10, 
+										 [](int y){ return y == 1988; }));
+  
+	return 0;
 }
